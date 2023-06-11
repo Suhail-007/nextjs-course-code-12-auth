ff04d85a2fe5d81f8cb30dfcb8d88acc5273a7c2
Suhail-007 <mohdsuhail51998@gmail.com>
2023-06-11 12:52:26 +0530

    clean all the code from final project

---
+++ components/profile/user-profile.js
@@ -0,0 +1,36 @@
+import ProfileForm from './profile-form';
+import classes from './user-profile.module.css';
+
+function UserProfile() {
+  // const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     if (!session) {
+  //       window.location.href = '/auth';
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   });
+  // }, []);
+
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>;
+  // }
+
+  async function changePasswordHandler(passwordData) {
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+
+  return (
+    <section className={classes.profile}>
+      <h1>Your User Profile</h1>
+      <ProfileForm onChangePassword={changePasswordHandler} />
+    </section>
+  );
+}
+
+export default UserProfile;
---
+++ pages/auth.js
@@ -0,0 +1,10 @@
+
+import { useEffect, useState } from 'react';
+
+import AuthForm from '../components/auth/auth-form';
+
+function AuthPage() {
+  return <AuthForm />;
+}
+
+export default AuthPage;
